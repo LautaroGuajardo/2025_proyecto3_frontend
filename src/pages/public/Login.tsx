26345d83c69c781c76015bf105ba586ade292937
Lautaro Guajardo <lautaroguajardo4@gmail.com>
2025-12-12 18:03:34 -0300

    Fix Login page.

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -125,14 +125,6 @@ const Login = () => {
                 </p>
               )}
             </div>
-            <div className="text-right mt-2">
-              <Link
-                to="/forgot-password"
-                className="text-sm text-blue-600 hover:underline"
-              >
-                ¿Olvidaste tu contraseña?
-              </Link>
-            </div>
 
             <Button type="submit" className="w-full h-13" disabled={isLoading}>
               Iniciar Sesión
